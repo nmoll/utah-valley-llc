960b248308e8b69b5176569f8274b104612e8f4e
Nathan Moll <nathan.moll93@gmail.com>
2022-03-02 14:08:59 -0700

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,4 @@
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    lib: {
-      entry: "src/utah-calendar-element.ts",
-      formats: ["es"],
-    },
-    rollupOptions: {
-      external: /^lit/,
-    },
-  },
-});
+export default defineConfig({});
