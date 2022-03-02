a240ec4483ff0f94b1c9531356484c1a0e975e71
Nathan Moll <nathan.moll93@gmail.com>
2022-03-02 14:03:39 -0700

    update build entry

---
+++ types/bible-class-leader-icon-element.d.ts
@@ -0,0 +1,9 @@
+import { LitElement } from "lit";
+export declare class UtahBibleClassLeaderElement extends LitElement {
+    render(): import("lit-html").TemplateResult<1>;
+}
+declare global {
+    interface HTMLElementTagNameMap {
+        "utah-bible-class-leader-icon": UtahBibleClassLeaderElement;
+    }
+}
---
+++ types/utah-calendar-element.d.ts
@@ -0,0 +1,15 @@
+import { LitElement } from "lit";
+import "./bible-class-leader-icon-element";
+import "./pianist-icon-element";
+import "./service-host-icon-element";
+export declare class UtahCalendarElement extends LitElement {
+    static styles: import("lit").CSSResult;
+    days: number[];
+    render(): import("lit-html").TemplateResult<1>;
+    private buildDays;
+}
+declare global {
+    interface HTMLElementTagNameMap {
+        "utah-calendar": UtahCalendarElement;
+    }
+}
