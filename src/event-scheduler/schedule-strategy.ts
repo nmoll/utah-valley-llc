f7ce431a973400bb6300a0d2038cf8f7b6b00ac6
Nathan Moll <nathan.moll93@gmail.com>
2022-07-28 15:44:41 -0600

    change sunday to 10am

--- src/event-scheduler/schedule-strategy.ts
+++ src/event-scheduler/schedule-strategy.ts
@@ -111,7 +111,7 @@ export class SundayMorningServiceStrategy implements ScheduleEventStrategy {
     return new ScheduleMorningHostStrategy();
   }
   getDescription(): string {
-    return "10:30am Service";
+    return "10am Service";
   }
 }
 
