4829da7ea4ee6edfec261606346d5400fe9f7fbc
Nathan Moll <nathan.moll93@gmail.com>
2024-05-28 22:23:35 -0600

    google sheets api key

--- src/element/calendar/bible-class-leader-store.ts
+++ src/element/calendar/bible-class-leader-store.ts
@@ -2,11 +2,13 @@ interface SpreadsheetResponse {
   values: string[][];
 }
 
+const sheetsApiKey = import.meta.env.VITE_GOOGLE_SHEETS_API_KEY;
+
 export class BibleClassLeaderStore {
   async getBibleClassLeaders(): Promise<Record<string, string>> {
     try {
       const response = await fetch(
-        "https://sheets.googleapis.com/v4/spreadsheets/1acLrliJ6GANqTUHPHodJ4qsz3Y-p2PV84C4t1bUQ3E4/values/Sheet1!A1:B2?key=AIzaSyAI0F6bQLi2pdECPQG09BvDsXHui_c9GSU"
+        `https://sheets.googleapis.com/v4/spreadsheets/1acLrliJ6GANqTUHPHodJ4qsz3Y-p2PV84C4t1bUQ3E4/values/Sheet1!A:B?key=${sheetsApiKey}`
       );
       const data: SpreadsheetResponse = await response.json();
       const result = data.values.reduce(
